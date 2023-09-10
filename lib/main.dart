b1c9e52848d5be495f8f38bec21e2ecbea16164d
AcropolisA <acropolis0401@gmail.com>
2023-09-10 20:18:15 +0900

    code in progress,,,

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(
+    child: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -15,54 +20,109 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: ManagerHome(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+class ManagerHomeController extends GetxController {
+  RxBool addMode = false.obs;
+  changeMode() => addMode.value = !addMode.value;
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+// ignore: must_be_immutable
+class ManagerHome extends ConsumerWidget {
+  ManagerHome({super.key});
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  List<Map<String, String>> mountingList = [
+    {'Path': '\\smb\\1\\data'},
+    {'Path': '\\smb\\2\\data'},
+    {'Path': '\\smb\\3\\data'},
+    {'Path': '\\smb\\4\\data'},
+    {'Path': '\\smb\\5\\data'},
+    {'Path': '\\smb\\6\\data'},
+    {'Path': '\\smb\\7\\data'},
+    {'Path': '\\smb\\8\\data'},
+    {'Path': '\\smb\\9\\data'},
+    {'Path': '\\smb\\10\\data'},
+    {'Path': '\\smb\\11\\data'},
+    {'Path': '\\smb\\12\\data'},
+    {'Path': '\\smb\\13\\data'},
+    {'Path': '\\smb\\14\\data'},
+  ];
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
+    final ManagerHomeController con = Get.put(ManagerHomeController());
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+              tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+            );
+          },
         ),
+        centerTitle: true,
+        elevation: 5,
+        title: const Text('Mount List'),
+        actions: [
+          Obx(
+            () => IconButton(
+              iconSize: 40.0,
+              onPressed: con.changeMode,
+              icon: con.addMode.value
+                  ? const Icon(CupertinoIcons.arrow_left)
+                  : const Icon(CupertinoIcons.add),
+            ),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SingleChildScrollView(
+                child: Expanded(
+                  flex: 0,
+                  child: Obx(
+                    () => Text(con.addMode.value ? 'ADD' : 'LIST'),
+                  ),
+                ),
+              ),
+              Obx(
+                () => Visibility(
+                  visible: con.addMode.value,
+                  child: Expanded(
+                    flex: 0,
+                    child: TextFormField(
+                      autofocus: true,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: ListView.separated(
+                    scrollDirection: Axis.vertical,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text((mountingList[index]['Path'].toString())),
+                      );
+                    },
+                    separatorBuilder: (context, index) => const Divider(),
+                    itemCount: mountingList.length),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }

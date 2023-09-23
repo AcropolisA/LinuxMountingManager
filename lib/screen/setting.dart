789e6381de756a3093ef46577f4ef13d67ba9f07
AcropolisA <acropolis0401@gmail.com>
2023-09-23 23:32:07 +0900

    UI update

--- lib/screen/setting.dart
+++ lib/screen/setting.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:linux_mounting_manager/popup/mount_manager_drawer.dart';
